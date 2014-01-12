e2d16e704f62897bfbeaffcd226fe722206e79f0
U-Parag-PC\Parag <Parag@Parag-PC.(none)>
2014-01-12 17:08:26 -0500

    Fixed bugs

--- lib/DBFile.cc
+++ lib/DBFile.cc
@@ -35,14 +35,14 @@ void DBFile::Load (Schema &f_schema, char *loadpath) {
 	Record* temp;
 	
 	while(temp->SuckNextRecord(&f_schema,tableFile)!=0)
-		this->Add(temp);
+		this->Add(*temp);
 		
 	fclose(tableFile);
 }
 
 int DBFile::Open (char *f_path) {
 	//TODO:metadata
-	this->File->Open(1,fpath);
+	this->file->Open(1,f_path);
 	pageIndex=1;
 }
 
@@ -57,7 +57,7 @@ int DBFile::Close () {
 
 	//TODO:metadata
 
-	return this->file->close();
+	return this->file->Close();
 	
 }
 
@@ -65,13 +65,13 @@ void DBFile::Add (Record &rec) {
 
 	//Consume rec
 	Record *write;
-	write->consume(rec);
+	write->Consume(&rec);
 	
-	if(writePage->append(write)==0)
+	if(writePage->Append(write)==0)
 	{		
-		this->file->AddPage(writePage,this->file->GetLength+1);
+		this->file->AddPage(writePage,(this->file->GetLength())+1);
 		this->writePage->EmptyItOut();
-		writePage->append(write);
+		writePage->Append(write);
 	}
 		
 }
@@ -96,7 +96,7 @@ int DBFile::GetNext (Record &fetchme) {
 	
 	
 	
-	fetchme = this->current;
+	fetchme = *this->current;
 	return 1;
 	
 }
@@ -109,14 +109,14 @@ int DBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
 	
 	while(result1==0||result2!=0){
 		
-		result2 = this->GetNext(this->read)
-		result1 = compare.Compare(this->read,literal,cnf);
+		result2 = this->GetNext(*this->current);
+		result1 = compare.Compare(this->current,&literal,&cnf);
 	
 	}
 	
 	if(result1==1)
 	{
-		fetchme = this->read;
+		fetchme = *this->current;
 		return 1;
 	}
 	
