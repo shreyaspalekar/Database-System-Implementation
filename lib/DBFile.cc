4edf4140a484872131b9393e95b0db632d42732e
Shreyas Ravindra Palekar <spalekar@lin114-00.cise.ufl.edu>
2014-01-16 12:41:31 -0500

    test

--- lib/DBFile.cc
+++ lib/DBFile.cc
@@ -58,9 +58,11 @@ int DBFile::Open (char *f_path) {
 
 void DBFile::MoveFirst () {
 //USE GetNext ? is two line code better??
-	this->file->GetPage(this->readPage,1); //TODO: check off_t type,  void GetPage (Page *putItHere, off_t whichPage)
-	this->readPage->GetFirst(this->current);//check if this->readPage is pointer
-
+//Should we let File.cc handle the boundry conditions??
+	//if(this->file->GetLength()>1){
+		this->file->GetPage(this->readPage,1); //TODO: check off_t type,  void GetPage (Page *putItHere, off_t whichPage)
+		this->readPage->GetFirst(this->current);//check if this->readPage is pointer
+	//}
 }
 
 int DBFile::Close () {
