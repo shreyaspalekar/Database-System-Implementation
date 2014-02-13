84d0a097d0dc87e365c699900a56203e8be5f2ce
Shreyas Palekar <shreyas.palekar@gmail.com>
2014-02-13 11:45:41 -0500

    Update BigQ.h

--- Phase 2/a2test/a2test/BigQ.h
+++ Phase 2/a2test/a2test/BigQ.h
@@ -12,35 +12,40 @@ class BigQ {
 
 	//DataStructures 
 	//Record temp to read in records from pipes
-	//Page buffer of size runlen to store read in records
-
 	
 	Record temp;
-	//Record *recordBuff;
-	//int pageLength = 0;//no of recrods per page
-	//Page *buffer;
 	DBFile *runs;
 	int no_runs;
 	pthread_t worker;
 	int page_Index;
 	
 	struct args_phase1_struct {                                                   
-		//check initialization
+		
+		//!!check initialization
+		
 		Pipe *input;
 		OrderMaker *sort_order;
 		int run_length;
-		//int pageLen = pageLength;
-		//Record *recordBuffer = recordBuff; 
 		int &num_runs = no_runs;
 		Record *temporary = &temp;
-		//Page *buf = buffer;
 		DBFile *run_buffer = runs;
 		char *file_path = "run";
+		
+		/*Deprecated: Replaced by DBFile , no need for indexing
+		//int pageLen = pageLength;
+		//Record *recordBuffer = recordBuff; 
+		//Page *buf = buffer;*/
 	
 	}args_phase1;
 
 	void* TPMMS_Phase1(void* arg);
 	void* TPMMS_Phase2(void* arg);
+	
+	/*Deprecated: Replaced by DBFile , no need for indexing
+	//Record *recordBuff;
+	//int pageLength = 0;//no of recrods per page
+	//Page *buffer;*/
+	
 
 public:
 
