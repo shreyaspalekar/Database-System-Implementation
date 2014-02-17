d86ca244688bf06b134d7926247e0ce759aa06d0
Shreyas Palekar <shreyas.palekar@gmail.com>
2014-02-17 10:14:38 -0500

    Update BigQ.cc

--- Phase 2/a2test/a2test/BigQ.cc
+++ Phase 2/a2test/a2test/BigQ.cc
@@ -77,6 +77,7 @@ void* BigQ::TPMMS_Phase1(void* arg){
 			Schema schema("catalog","lineitem");
 			cout<<"Record Buffer: ";
 			for(int i=0;i<count;i++){
+				cout<<"Printing record: "<<i<<"\n";
 				(*(record_Buffer+i))->Print(&schema);
 			}
 			cout << "read "<<z<<" records\n";
