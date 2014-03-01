6adb70452803ac6c3d4867ab568b88cbbd91762f
Shreyas Palekar <shreyas.palekar@gmail.com>
2014-03-01 15:51:15 -0500

    Base

--- Phase2.1/Source/DBFile.h
+++ Phase2.1/Source/DBFile.h
@@ -12,6 +12,29 @@ typedef enum {heap, sorted, tree} fType;
 
 // stub DBFile header..replace it with your own DBFile.h 
 
+class GenericDBFile{
+
+public:
+	
+	GenericDBFile();
+	
+	virtual int Create (char *fpath, fType file_type, void *startup) = 0;
+	virtual int Open (char *fpath) = 0;
+	virtual int Close () = 0;
+
+	virtual void Load (Schema &myschema, char *loadpath) = 0;
+
+	virtual void MoveFirst () = 0;
+	virtual void Add (Record &addme) = 0;
+	virtual int GetNext (Record &fetchme) = 0;
+	virtual int GetNext (Record &fetchme, CNF &cnf, Record &literal) = 0;
+	
+	virtual ~GenericDBFile();
+
+}
+
+
+
 class DBFile {
 
 public:
